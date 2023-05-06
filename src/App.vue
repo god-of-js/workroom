2d5fd3621c3c919accc559c0e48e06fa23c1910a
god-of-js <henryeze019@gmail.com>
2023-05-06 10:08:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <main id="app">
+    <RouterView />
+  </main>
+</template>
+
+<style lang="scss">
+#app {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis esse at molestias explicabo
+    iste modi culpa, quo aliquam cupiditate laboriosam neque dolore fugiat quidem optio quos ipsa
+    exercitationem repellendus qui?
+  </main>
+</template>
+
+<styled lang="scss" scoped>
+main {
+  background: red;
+}
+</styled>
